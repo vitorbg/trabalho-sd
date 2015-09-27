3df53157e606769a1d7a3737780f5997e67ed9c0
Vitor <vitorbg92@gmail.com>
2015-09-27 18:31:36 -0300

    Testes com metodo POST

--- testeV/Cliente/src/cliente/Cliente.java
+++ testeV/Cliente/src/cliente/Cliente.java
@@ -8,33 +8,47 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 /*  Passe dois argumentos: url + porta
-*   Exemplos:
-*   localhost 80
-*   www.uol.com.br 80
-*   localhost 8089
-*/
-
+ *   Exemplos:
+ *   localhost 80
+ *   www.uol.com.br 80
+ *   localhost 8089
+ */
 public class Cliente {
+
     public static void main(String[] args) throws IOException {
-    Socket server;
-    if(args[0].equals("localhost") || args[0].equals("127.0.0.1"))
-    {
-        server = new Socket(InetAddress.getLocalHost(), Integer.parseInt(args[1]));
-    }
-    else{
-        server = new Socket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+
+        Socket server;
+
+        if (args[0].equals("localhost") || args[0].equals("127.0.0.1")) {
+            server = new Socket(InetAddress.getLocalHost(), Integer.parseInt(args[1]));
+        } else {
+            server = new Socket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+        }
+
+        PrintWriter pw = new PrintWriter(server.getOutputStream());
+        pw.println("POST / HTTP/1.1");
+        pw.println("Host: " + args[0]);
+        pw.println("name=vitor"+"&"+"lastname=goncalves");
+        pw.println("");
+        pw.flush();
+        BufferedReader br = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        String t;
+        while ((t = br.readLine()) != null) {
+            System.out.println(t);
+        }
+        br.close();
+
+//        PrintWriter pw = new PrintWriter(server.getOutputStream());
+//        pw.println("GET / HTTP/1.1");
+//        pw.println("Host: " + args[0]);
+//        pw.println("");
+//        pw.flush();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(server.getInputStream()));
+//        String t;
+//        while ((t = br.readLine()) != null) {
+//            System.out.println(t);
+//        }
+//        br.close();
     }
 
-    PrintWriter pw = new PrintWriter(server.getOutputStream());
-    pw.println("GET / HTTP/1.1");
-    pw.println("Host: "+args[0]);
-    pw.println("");
-    pw.flush();
-    BufferedReader br = new BufferedReader(new InputStreamReader(server.getInputStream()));
-    String t;
-    while((t = br.readLine()) != null) 
-        System.out.println(t);
-    br.close();
-    }
- 
 }
--- testeV/Servidor/src/servidor/Servidor.java
+++ testeV/Servidor/src/servidor/Servidor.java
@@ -18,195 +18,182 @@ import java.util.Date;
 import java.util.StringTokenizer;
 
 /*  Passe tres argumentos: pastaraiz + porta + nome do arquivo da pagina contido na pasta raiz
-*   Exemplos:
-*   C:/httdocs 8089 index.html
-*   . 80 pagina.html                          // . como parametro retorna a pasta do projeto
-*   C:/Servidor 80 index.html
-*/
+ *   Exemplos:
+ *   C:/httdocs 8089 index.html
+ *   . 80 pagina.html                          // . como parametro retorna a pasta do projeto
+ *   C:/Servidor 80 index.html
+ */
+public class Servidor implements Runnable {
 
-public class Servidor implements Runnable
-{
+    static File raiz;
+    static String arq_pagina;
+    static int porta;
+    Socket connect;
 
-  static File raiz;
-  static String arq_pagina;
-  static int porta; 
-  Socket connect;
-
-
-  public Servidor(Socket connect)
-  {
-    this.connect = connect;
-    System.out.println("[Servidor] Cliente conectado: " + connect.getInetAddress().getHostAddress() + ".");
-  }
-
-  public static void main(String[] args)
-  {
-    raiz = new File(args[0]);
-    porta = Integer.parseInt(args[1]);  
-    arq_pagina = args[2];
-    System.out.println("Sistemas Distribuidos: Projeto 1");
-    System.out.println("[Servidor] Iniciando..");
-    try
-    {
-      ServerSocket serverConnect = new ServerSocket(porta);
-      System.out.println("[Servidor] Escutando a porta "+porta + ".");
-      while (true)
-      {
-        Servidor server = new Servidor(serverConnect.accept());     
-        Thread threadRunner = new Thread(server);
-        threadRunner.start(); 
-      }
+    public Servidor(Socket connect) {
+        this.connect = connect;
+        System.out.println("[Servidor] Cliente conectado: " + connect.getInetAddress().getHostAddress() + ".");
+        System.out.println("[Servidor] Cliente conectado: " + connect.getInetAddress().getCanonicalHostName() + ".");
     }
-    catch (IOException e)
-    {
-      System.err.println("Erro: " + e);
+
+    public static void main(String[] args) {
+        raiz = new File(args[0]);
+        porta = Integer.parseInt(args[1]);
+        arq_pagina = args[2];
+        System.out.println("Sistemas Distribuidos: Projeto 2");
+        System.out.println("[Servidor] Iniciando..");
+        try {
+            ServerSocket serverConnect = new ServerSocket(porta);
+            System.out.println("[Servidor] Escutando a porta " + porta + ".");
+            while (true) {
+                Servidor server = new Servidor(serverConnect.accept());
+                Thread threadRunner = new Thread(server);
+                threadRunner.start();
+            }
+        } catch (IOException e) {
+            System.err.println("Erro: " + e);
+        }
     }
-  }
 
-  public void run()
-  {
-    BufferedReader in = null;
-    PrintWriter out = null;
-    BufferedOutputStream dOut = null;
-    String fileRequested = null;
-    try
-    {
-      in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-      out = new PrintWriter(connect.getOutputStream()); /* Enviar cabecalhos ao cliente */
-      dOut = new BufferedOutputStream(connect.getOutputStream()); /* Enviar dados ao cliente */
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        BufferedOutputStream dOut = null;
+        String fileRequested = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            out = new PrintWriter(connect.getOutputStream()); /* Enviar cabecalhos ao cliente */
 
-      String input = in.readLine(); /* GET / HTTP/1.1 */
-      StringTokenizer parse = new StringTokenizer(input);
-      String method = parse.nextToken().toUpperCase(); /* GET */
+            dOut = new BufferedOutputStream(connect.getOutputStream()); /* Enviar dados ao cliente */
 
-      fileRequested = parse.nextToken().toLowerCase(); /* / */
+            String input = in.readLine(); /* GET / HTTP/1.1 */
 
-      if (fileRequested.endsWith("/"))
-      {
-        fileRequested += arq_pagina;  // fileReq: concatena / com a index.html.. Resultado: /index.html
-      }
-      File file = new File(raiz, fileRequested);
-      int fileLength = (int)file.length(); 
-      
+            System.out.println("Cabeçalho: " + input);
 
-      String content = getContentType(fileRequested); 
+            StringTokenizer parse = new StringTokenizer(input);
+            String method = parse.nextToken().toUpperCase(); /* GET */
 
-      if(method.equals("GET"))
-      {
-        FileInputStream fileIn = null;
-        byte[] fileData = new byte[fileLength];
+            System.out.println("Metodo: " + method);
 
-        try
-        {
-          fileIn = new FileInputStream(file);
-          fileIn.read(fileData);
+            fileRequested = parse.nextToken().toLowerCase(); /* / */
+
+            if (fileRequested.endsWith("/")) {
+                fileRequested += arq_pagina;  // fileReq: concatena / com a index.html.. Resultado: /index.html
+            }
+            File file = new File(raiz, fileRequested);
+            int fileLength = (int) file.length();
+
+            String content = getContentType(fileRequested);
+
+            if (method.equals("GET")) {
+                FileInputStream fileIn = null;
+                byte[] fileData = new byte[fileLength];
+
+                try {
+                    fileIn = new FileInputStream(file);
+                    fileIn.read(fileData);
+                } finally {
+                    close(fileIn);
+                }
+                /* Cabecalho */
+                out.println("HTTP/1.0 200 OK");
+                out.println("Date: " + new Date());
+                out.println("Content-type: " + content);
+                out.println("Content-length: " + file.length());
+                out.println();
+                out.flush();
+
+                dOut.write(fileData, 0, fileLength);
+                dOut.flush();
+            }
+            if (method.equals("POST")) {
+                FileInputStream fileIn = null;
+                byte[] fileData = new byte[fileLength];
+
+                try {
+                    fileIn = new FileInputStream(file);
+                    fileIn.read(fileData);
+                } finally {
+                    close(fileIn);
+                }
+                String input2 = in.readLine();
+                System.out.println("  "+input2);
+                /* Cabecalho */
+                out.println("HTTP/1.0 200 OK");
+                out.println("Date: " + new Date());
+                out.println("Content-type: " + content);
+                out.println("Content-length: " + file.length());
+                out.println();
+                out.flush();
+
+                dOut.write(fileData, 0, fileLength);
+                dOut.flush();
+            }
+
+        } catch (FileNotFoundException fnfe) {
+            fileNotFound(out, fileRequested);
+        } catch (IOException ioe) {
+            System.err.println("Server Error: " + ioe);
+        } finally {
+            close(in); //fecha character input stream
+            close(out); //fecha character output stream
+            close(dOut); //fecha binary output stream
+            close(connect); //fecha socket connection
+
         }
-        finally
-        {
-          close(fileIn); 
-        }
-        /* Cabecalho */
-        out.println("HTTP/1.0 200 OK");
+    }
+
+    private void fileNotFound(PrintWriter out, String file) {
+        out.println("HTTP/1.0 404 File Not Found");
         out.println("Date: " + new Date());
-        out.println("Content-type: " + content);
-        out.println("Content-length: " + file.length());
-        out.println(); 
-        out.flush(); 
-
-        dOut.write(fileData,0,fileLength); 
-        dOut.flush(); 
-      }
-
+        out.println("Content-Type: text/html");
+        out.println();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>File Not Found</TITLE>" + "</HEAD>");
+        out.println("<BODY>");
+        out.println("<H2>404 File Not Found: " + file + "</H2>");
+        out.println("</BODY>");
+        out.println("</HTML>");
+        out.flush();
     }
-    catch (FileNotFoundException fnfe)
-    {
-      fileNotFound(out, fileRequested);
+
+
+    /*
+     Retorna o conteudo de acordo com a extensao do arquivo
+     */
+    private String getContentType(String fileRequested) {
+        if (fileRequested.endsWith(".htm") || fileRequested.endsWith(".html")) {
+            return "text/html";
+        } else {
+            return "text/plain";
+        }
     }
-    catch (IOException ioe)
-    {
-      System.err.println("Server Error: " + ioe);
+
+
+    /*
+     da .close() na stream
+     */
+    public void close(Object stream) {
+        if (stream == null) {
+            return;
+        }
+
+        try {
+            if (stream instanceof Reader) {
+                ((Reader) stream).close();
+            } else if (stream instanceof Writer) {
+                ((Writer) stream).close();
+            } else if (stream instanceof InputStream) {
+                ((InputStream) stream).close();
+            } else if (stream instanceof OutputStream) {
+                ((OutputStream) stream).close();
+            } else if (stream instanceof Socket) {
+                ((Socket) stream).close();
+            } else {
+                System.err.println("Tentativa falha de fechar a stream: " + stream);
+            }
+        } catch (Exception e) {
+            System.err.println("Falha fechando a stream: " + e);
+        }
     }
-    finally
-    {
-      close(in); //fecha character input stream
-      close(out); //fecha character output stream
-      close(dOut); //fecha binary output stream
-      close(connect); //fecha socket connection
-
-    }
-  }
-
-  private void fileNotFound(PrintWriter out, String file)
-  {
-    out.println("HTTP/1.0 404 File Not Found");
-    out.println("Date: " + new Date());
-    out.println("Content-Type: text/html");
-    out.println();
-    out.println("<HTML>");
-    out.println("<HEAD><TITLE>File Not Found</TITLE>" + "</HEAD>");
-    out.println("<BODY>");
-    out.println("<H2>404 File Not Found: " + file + "</H2>");
-    out.println("</BODY>");
-    out.println("</HTML>");
-    out.flush();
-  }
-
-
-   /*
-        Retorna o conteudo de acordo com a extensao do arquivo
-   */
-
-  private String getContentType(String fileRequested)
-  {
-    if (fileRequested.endsWith(".htm") || fileRequested.endsWith(".html"))
-    {
-      return "text/html";
-    }
-    else
-    {
-      return "text/plain";
-    }
-  }
-
-
-   /*
-        da .close() na stream
-   */
-  public void close(Object stream)
-  {
-    if (stream == null)
-      return;
-
-    try
-    {
-      if (stream instanceof Reader)
-      {
-        ((Reader)stream).close();
-      }
-      else if (stream instanceof Writer)
-      {
-        ((Writer)stream).close();
-      }
-      else if (stream instanceof InputStream)
-      {
-        ((InputStream)stream).close();
-      }
-      else if (stream instanceof OutputStream)
-      {
-        ((OutputStream)stream).close();
-      }
-      else if (stream instanceof Socket)
-      {
-        ((Socket)stream).close();
-      }
-      else
-      {
-        System.err.println("Tentativa falha de fechar a stream: " + stream);
-      }
-    }
-    catch (Exception e)
-    {
-      System.err.println("Falha fechando a stream: " + e);
-    }
-  }
 }
