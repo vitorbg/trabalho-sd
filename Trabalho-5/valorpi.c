1c79515bc08ccb78856df5a807998e23d9817992
Vitor Gonçalves <vitorbg92@gmail.com>
2015-12-16 23:39:09 -0200

    Trabalho 5 FINAL

--- Trabalho-5/valorpi.c
+++ Trabalho-5/valorpi.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX_STRING 100
 
@@ -25,8 +26,70 @@ int main(int argc, char** argv) {
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+	int numeroLinhas;
+	int numeroColunas;
+	int numeroMax;
+	int fatorCalculo; 
+	char fatorPGM[10];
+	int tamanhoVetor;
+	int quantidadePartes;
+
+
+	FILE *file;
+	int *vetorImagem;
+	int *vetorImagemResultante;
+	int *bufferRecv;
+	int i;
+	int j;
+
+
 	int quantidadePontos  = 20;
-	int buffer;
+
+
+	if(world_rank == 0) {
+		fatorCalculo = atoi(argv[1]);
+		file = fopen(argv[2],"r");
+		fscanf(file, "%[^\n]", fatorPGM);
+		fgetc(file);
+		fscanf(file, "%d", &numeroColunas);
+		fscanf(file, "%d", &numeroLinhas);
+		fscanf(file, "%d", &numeroMax);
+    		printf("Leitura da Imagem \n");
+    		printf("Colunas: %d\n", numeroColunas);
+    		printf("Linhas: %d\n", numeroLinhas);
+    		printf("Max: %d\n", numeroMax);
+    		printf("Fator de Calculo: %d\n", fatorCalculo);
+
+//-------
+		tamanhoVetor = numeroLinhas * numeroColunas;
+    		printf("Tamanho do Vetor: %d\n", tamanhoVetor);
+
+		quantidadePartes = abs((tamanhoVetor/world_size));
+
+		vetorImagem = (int *) malloc (tamanhoVetor * sizeof(int));
+		vetorImagemResultante = (int *) malloc (tamanhoVetor * sizeof(int));
+
+    		printf("Quantidade de Partes: %d\n", quantidadePartes);
+
+		vetorImagemResultante[0]=0;
+
+
+		//Preenche Vetor Imagem
+		for(i = 0; i < tamanhoVetor; i++) {
+			fscanf(file, "%d", &vetorImagem[i]);
+		}
+		fclose(file);
+
+
+    		printf("GRANDE VETOR \n");
+		for (i = 0; i < tamanhoVetor; i++){
+			printf("%d\n ", vetorImagem[i]);
+		}
+    		printf("GRANDE VETOR FIM\n");
+	}
+
+
+
 
 	// We are assuming at least 2 processes for this task
   	if (world_size < 2) {
@@ -36,34 +99,59 @@ int main(int argc, char** argv) {
 
 
 	MPI_Bcast(&quantidadePontos, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	//if(world_rank == 0) {
-		//MPI_Bcast(&quantidadePontos, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  		//int i;
-		//printf("Entrei no processo zero ! \n");	
-	  	//for(i = 1; i < world_size; i++) {
-			//printf("Enviando ! \n");	
-		  	//MPI_Send(&quantidadePontos, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-  		//}
-  	//}
+	MPI_Bcast(&numeroColunas, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&numeroLinhas, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&numeroMax, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&fatorCalculo, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&tamanhoVetor, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&quantidadePartes, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	bufferRecv = (int *) malloc ((quantidadePartes)* sizeof(int));
+	//MPI_Scatter(&sendbuf,sendcnt,sendtype,&recvbuf,recvcnt,recvtype,source,comm);
+	MPI_Scatter(vetorImagem, quantidadePartes, MPI_INT, bufferRecv, quantidadePartes,MPI_INT,0,MPI_COMM_WORLD);
+
+  	
+
 
 
 
 	//int number;
-  	if (world_rank != 0) {
-    	// If we are rank 0, set the number to -1 and send it to process 1
-    	//sprintf(greeting, "Greetings from process %d of %d!", world_rank, world_size);
-    	//MPI_Send(&greeting, strlen(greeting) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-			
-		printf("Antes de receber %d! \n", world_rank);	
-    		MPI_Recv(&buffer, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("RECEBI: %d \n",buffer);
-  	} //else {
-  	//	int i;
-	  //	for(i = 1; i < world_size; i++){
-		//  	MPI_Recv(&greeting, MAX_STRING, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		//	printf("Process 0 received \'%s\' from process %d\n", greeting, i);
-  	//}
-  
- // }
+  	if (1) {
+    		printf("world_rank %d \n",world_rank);
+    		printf("Colunas: %d\n", numeroColunas);
+    		printf("Linhas: %d\n", numeroLinhas);
+    		printf("Max: %d\n", numeroMax);
+    		printf("Fator de Calculo: %d\n", fatorCalculo);
+    		printf("Tamanho do Vetor: %d\n", tamanhoVetor);
+    		printf("Quantidade de Partes: %d\n", quantidadePartes);
+		printf("To no world  ");
+		for(i=0; i < quantidadePartes-1;i++) {
+			if (abs((bufferRecv[i] - bufferRecv[i+1])) < fatorCalculo ) {
+				bufferRecv[i] = numeroMax;
+			}
+			else {
+				bufferRecv[i] = 0;
+			} 
+		}
+  	}
+ 
+	//MPI_Gather(&sendbuf,sendcnt,sendtype,&recvbuf,recvcount,recvtype,dest,comm);
+	MPI_Gather(bufferRecv,quantidadePartes,MPI_INT,vetorImagem,quantidadePartes,MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (world_rank == 0) {
+		printf("GRANDE VETOR \n");
+		j=0;
+		int k=0;
+		for (i = 0; i < numeroLinhas; i++){
+			for(j=0; j< numeroColunas;j++){
+				printf(" %d ", vetorImagem[k]);
+				k++;
+			}
+			printf("\n");
+		}
+    		printf("\nGRANDE VETOR FIM\n");
+
+  	
+	}
   	MPI_Finalize();
 }
